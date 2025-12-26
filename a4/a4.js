34bd119d1238f755b46ddfbf2167e1374caa6e39
mohanad diaa <mohanaddata4@gmail.com>
2025-12-26 15:56:37 +0200

    a4

--- a4/a4.js
+++ a4/a4.js
@@ -2,30 +2,27 @@ import express from "express";
 import path from "path";
 import fs from "fs";
 import e from "express";
-import { resourceUsage } from "process";
+import { log } from "console";
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.post("/user", (req, res) => {
-  let result = add_user(req,res);
-
-});
-
-function add_user(req,res) {
-  let data = req.body;
-  let check = checkMail(data.email);
-  if (!check){
-    let file = JSON.parse(fs.readFileSync(path.resolve('data.json')));
-    let id = add_id(file);
-    data.id = id ;
-    file.push(data);
-    fs.writeFileSync(path.resolve('data.json'),JSON.stringify(file,null,2))
-    res.status(200).send('user added');
-
+app.post("/user",
+  (req,res,next)=>{
+    let data = req.body;
+    let check = checkMail(data.email);
+    if ( check == undefined ){
+      next();
     }
-}
+    else {
+      res.status(400).send("use anthor email");
+    }
+  },
+  (req, res) => {
+  let result = add_user(req,res);
+  res.status(201).send(result);
+});
 
 function checkMail(email) {
   let file = JSON.parse(fs.readFileSync(path.resolve("data.json")));
@@ -36,6 +33,16 @@ function checkMail(email) {
   return find;
 }
 
+function add_user(req,res) {
+  let data = req.body ;  
+    let file = JSON.parse(fs.readFileSync(path.resolve('data.json')));
+    let id = add_id(file);
+    data.id = id ;
+    file.push(data);
+    fs.writeFileSync(path.resolve('data.json'),JSON.stringify(file,null,2))
+    return 'user added';
+}
+
 function add_id(data) {
   let file = JSON.parse(fs.readFileSync(path.resolve("data.json")));
   let id = file.length;
@@ -43,45 +50,42 @@ function add_id(data) {
   else return id++;
 }
 
-app.patch("/user/:id", (req, res) => {
-  let result = update_user(req);
+app.patch("/user/:id", 
+  (req, res) => {
+  let result = update_user(req);  
   res.status(200).send(result);
+  res.end();
 });
 
 function update_user(req) {
-  let id = req.params;
-  let new_data = "";
-  req.on("data", (chunks) => {
-    new_data += chunks;
-  });
-
-  req.on("end", () => {
-    new_data = JSON.parse(new_data);
-    let data = JSON.parse(fs.readFileSync(path.resolve("data.json")));
-
-    let idx = data.findIndex((e) => {
-      return e.id == Object.values(id)[0];
-    });
-    if (idx != -1) {
-      if (Object.keys(new_data)[0] == "name") {
-        data[idx].name = new_data.name;
-      } else if (Object.keys(new_data)[0] == "age") {
-        data[idx].age = new_data.age;
-      } else if (Object.keys(new_data)[0] == "email") {
-        data[idx].email = new_data.email;
-      }
-      fs.writeFileSync(
-        path.resolve("data.json"),
-        JSON.stringify(data),
-        null,
-        2
-      );
-
-      return "user updated";
-    } else {
-      return "user not found";
+  
+  let id = req.params.id ;
+  let update =  Object.keys(req.body)[0];
+  let data = JSON.parse(fs.readFileSync(path.resolve('data.json')));
+  let user = data.findIndex((e)=>{
+    return e.id == id ;
+  })
+  if ( user === -1 ){
+    return 'user is not found'
+  }
+  else {
+    if (update == "name" ){
+      data[user].name = req.body.name;
     }
-  });
+    else if (update == "age" ){
+      data[user].age = req.body.age;
+    }
+    else if (update == "email" ){
+      data[user].email = req.body.email;
+    }
+    else {
+      return 'cannot find property';
+    }
+    fs.writeFileSync(path.resolve('data.json'),JSON.stringify(data),null,2);
+
+    return 'user updated';
+  
+  }
 }
 
 app.delete("/user/:id", (req, res) => {
--- a4/tempCodeRunnerFile.js
+++ a4/tempCodeRunnerFile.js
@@ -1,17 +1,4 @@
-function add_user(req,res){
-  
-  let data = '' ;
-  req.on('data',(chunks)=>{
-    console.log('*');
-    data += chunks;
-    })
-
-    req.on('end',()=>{
-      data = JSON.parse(data);
-      let check = checkMail(data);
-      data.id = add_id(data);        
-        
-      if (!check){
-        let file = JSON.parse(fs.readFileSync(path.resolve('data.json')));
-        file.push(data);
-        fs.writeFileSync(path.resolve('data.json'),JSON.stringify(file,null,2.5))}})}
+  if ( user === -1 ){
+    return 'user is not found'
+  }
+  else {
